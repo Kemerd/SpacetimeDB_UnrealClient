ecc8e9ad08958a0c367e9e7d9a41189cc519f506
D Everett Hinton <Dragonfabledonny@hotmail.com>
2025-05-08 20:15:52 -0700

    Update SpacetimeDB_UnrealClient.Build.cs

--- SpacetimeDB_UnrealClient/Source/SpacetimeDB_UnrealClient/SpacetimeDB_UnrealClient.Build.cs
+++ SpacetimeDB_UnrealClient/Source/SpacetimeDB_UnrealClient/SpacetimeDB_UnrealClient.Build.cs
@@ -32,7 +32,9 @@ public class SpacetimeDB_UnrealClient : ModuleRules
                 "Engine",
                 "InputCore",
                 "Networking",
-                "Sockets"
+                "Sockets",
+                "Json",
+                "JsonUtilities"
                 // ... add other public dependencies that you statically link with here ...
             }
         );
@@ -51,20 +53,119 @@ public class SpacetimeDB_UnrealClient : ModuleRules
             }
         );
         
-        // Build Rust code on Windows and non-Windows platforms
-        string rustBuildScript = IsWindows(Target) ? "build-rust-win.bat" : "build-rust.sh";
-        string rustBuildScriptPath = Path.Combine(ModuleDirectory, "../../rust", rustBuildScript);
+        // Only build Rust code if not in a CI environment that has pre-built the libraries
+        // This can be controlled by setting an environment variable like SPACETIMEDB_SKIP_RUST_BUILD
+        bool skipRustBuild = Environment.GetEnvironmentVariable("SPACETIMEDB_SKIP_RUST_BUILD") == "1";
         
-        // Add Rust library path to link against
-        string rustLibPath = Path.Combine(ModuleDirectory, "../../rust/target/release");
-        PublicAdditionalLibraries.Add(Path.Combine(rustLibPath, "spacetimedb_client.lib"));
+        if (!skipRustBuild)
+        {
+            // Determine build script based on platform
+            string rustBuildScript = "";
+            string libExtension = "";
+            
+            if (Target.Platform == UnrealTargetPlatform.Win64)
+            {
+                rustBuildScript = "build-rust-win.bat";
+                libExtension = "lib";
+            }
+            else if (Target.Platform == UnrealTargetPlatform.Mac)
+            {
+                rustBuildScript = "build-rust.sh";
+                libExtension = "a";
+            }
+            else if (Target.Platform == UnrealTargetPlatform.Linux)
+            {
+                rustBuildScript = "build-rust.sh";
+                libExtension = "a";
+            }
+            else
+            {
+                throw new BuildException("Unsupported platform for SpacetimeDB Unreal Client");
+            }
+            
+            // Full path to the build script
+            string rustBuildScriptPath = Path.Combine(ModuleDirectory, "../../rust", rustBuildScript);
+            
+            // Make sure the script is executable on Unix platforms
+            if (Target.Platform != UnrealTargetPlatform.Win64)
+            {
+                try
+                {
+                    Process chmod = new Process();
+                    chmod.StartInfo.FileName = "/bin/chmod";
+                    chmod.StartInfo.Arguments = $"+x \"{rustBuildScriptPath}\"";
+                    chmod.StartInfo.UseShellExecute = false;
+                    chmod.Start();
+                    chmod.WaitForExit();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Warning: Failed to set executable permission on build script: {e.Message}");
+                }
+            }
+            
+            // Add pre-build step to build Rust library
+            PreBuildSteps.Add(rustBuildScriptPath);
+            
+            // Log information about the build
+            Console.WriteLine($"SpacetimeDB: Running Rust build script: {rustBuildScriptPath}");
+        }
+        else
+        {
+            Console.WriteLine("SpacetimeDB: Skipping Rust build as SPACETIMEDB_SKIP_RUST_BUILD is set");
+        }
         
-        // Add pre-build step to build Rust library
-        PreBuildSteps.Add(rustBuildScriptPath);
-    }
-    
-    private bool IsWindows(ReadOnlyTargetRules Target)
-    {
-        return Target.Platform == UnrealTargetPlatform.Win64;
+        // Add Rust library path and header include path
+        string rustTargetPath = Path.Combine(ModuleDirectory, "../../rust/target");
+        string configFolder = (Target.Configuration == UnrealTargetConfiguration.Debug ||
+                              Target.Configuration == UnrealTargetConfiguration.DebugGame) ? "debug" : "release";
+        string rustLibPath = Path.Combine(rustTargetPath, configFolder);
+        
+        // Determine the correct library name based on platform
+        string libName = "";
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            libName = "spacetimedb_client.lib";
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Mac || Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            libName = "libspacetimedb_client.a";
+        }
+        
+        // Add the Rust library to link against
+        string fullLibPath = Path.Combine(rustLibPath, libName);
+        if (File.Exists(fullLibPath))
+        {
+            PublicAdditionalLibraries.Add(fullLibPath);
+            Console.WriteLine($"SpacetimeDB: Adding Rust library: {fullLibPath}");
+        }
+        else
+        {
+            // In CI/CD environments, we might have pre-built libs in a different location
+            string ciLibPath = Environment.GetEnvironmentVariable("SPACETIMEDB_RUST_LIB_PATH");
+            if (!string.IsNullOrEmpty(ciLibPath) && File.Exists(Path.Combine(ciLibPath, libName)))
+            {
+                PublicAdditionalLibraries.Add(Path.Combine(ciLibPath, libName));
+                Console.WriteLine($"SpacetimeDB: Adding CI/CD provided Rust library from: {ciLibPath}");
+            }
+            else
+            {
+                // Still add the path even if the file doesn't exist yet, as it will be created during build
+                PublicAdditionalLibraries.Add(fullLibPath);
+                Console.WriteLine($"SpacetimeDB: Expected Rust library will be built at: {fullLibPath}");
+            }
+        }
+        
+        // On macOS, we need to add these frameworks
+        if (Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            PublicFrameworks.AddRange(new string[] { "Security", "CoreFoundation" });
+        }
+        
+        // On Linux, we might need additional libraries
+        if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            PublicSystemLibraries.AddRange(new string[] { "ssl", "crypto" });
+        }
     }
 } 
